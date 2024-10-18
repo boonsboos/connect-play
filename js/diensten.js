f5bc1160a9fbd28225dc43ac3c48099da0facd58
Mingull <60094450+Mingull@users.noreply.github.com>
2024-10-18 13:44:17 +0200

    Added comment to JS

--- js/diensten.js
+++ js/diensten.js
@@ -1,3 +1,4 @@
+// this doesn't change so therefor it's a constant variable
 const diensten = [
 	{
 		id: 1,
@@ -27,20 +28,28 @@ const diensten = [
 
 const dienstenContainer = document.getElementById("diensten")
 
+// loop through the diensten array and create an element for each dienst 
 for (var i = 0; i < diensten.length; i++) {
 	dienstenContainer.appendChild(createDienstElement(diensten[i]))
 }
 
+// create a dienst element with the given dienst object
 function createDienstElement(dienst) {
+	// create a new div element
 	const dienstElement = document.createElement("div")
-	dienstElement.classList.add("dienst")
+	// add classes to the dienst element, each class is its own string
+	dienstElement.classList.add("dienst", "col-6", "flex-col", "justify-center", "mb-col-12")
 
+	// set the innerHTML of the dienst element just like .appendChild() does
+	// this is a template string, it allows you to use variables in the string
 	dienstElement.innerHTML = `
         <img src="${dienst.image}" alt="${dienst.title}" />
-        <h2>${dienst.title}</h2>
-        <p>${dienst.description}</p>
-        <button class="button">Bekijk</button>
+        <div class="flex px-15">
+			<h2>${dienst.title}</h2>
+        	<p>${dienst.description}</p>
+        	<button class="button">Bekijk</button>
+		</div>
     `
-
+	// return the dienst element
 	return dienstElement
 }
