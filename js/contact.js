3efa09b1fe9120d53955c0d0f37abc5abadb1cc7
mastermooss <60898036+mastermooss@users.noreply.github.com>
2024-10-20 16:08:27 +0200

    Added footer to pages - clean code (#27)
    * Style/diensten (#23)
    
    * added homepage styling
    
    * small fix, could not figure out the misaligned images
    
    * 'Neem contact op' button was not centered
    
    * Diensten filled with data
    
    ---------
    
    Co-authored-by: boons <57451966+boonsboos@users.noreply.github.com>
    
    * Change usp format and add color to boxen contact/overonspage
    
    ---------
    
    Co-authored-by: Mingull <60094450+Mingull@users.noreply.github.com>
    Co-authored-by: boons <57451966+boonsboos@users.noreply.github.com>
    Co-authored-by: mastermooss <mastermoos@gmail.com>

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,24 @@
+// Find elment with ID "form-box" and listen to event "submit"
 document.getElementById('form-box').addEventListener('submit', function (event) {
+    // Get all inputfields and textareafields
     let inputFields = this.querySelectorAll('#form-box input, #form-box textarea');
+    
+    // create a boolean for the check
     let valueFields = true;
 
     for (let i = 0; i < inputFields.length; i++) {
+        // Delete all class emptyField
         inputFields[i].classList.remove('emptyField');
-
+        
+        // Check if let inputFields is empty. If empty, add class emptyField
+        // set valueFields to false
         if (inputFields[i].value === "") {
             inputFields[i].classList.add('emptyField');
             valueFields = false;
         }
     }
+
+    // Check if valueFields is false
     if (!valueFields) {
         event.preventDefault();
         alert("Vul alle velden in.");
