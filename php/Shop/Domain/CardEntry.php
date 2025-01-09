6ef1f3b88492791297517d93e3faca7c4915794a
mastermooss <mastersmoos@gmail.com>
2025-01-09 15:30:04 +0100

    Added new files, add class CardEntry

---
+++ php/Shop/Domain/CardEntry.php
@@ -0,0 +1,22 @@
+<?php 
+
+class CardEntry {
+    private int $copies;
+
+    function __construct($copies)
+    {
+     $this->copies = $copies;   
+    }
+    
+    public function addCopy (){
+
+    }
+
+    public function removeCopy() {
+
+    }
+
+    public function toggleWorkshop() {
+
+    }
+}
