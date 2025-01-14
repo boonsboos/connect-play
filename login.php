1bc93adba7742272dc1feabfac0e0f4762d3fac2
Mingull <60094450+Mingull@users.noreply.github.com>
2025-01-14 21:18:27 +0100

    Updated username -> email

--- login.php
+++ login.php
@@ -2,22 +2,22 @@
 <img class="banner-img" src="images/bannerImg.jpg" alt="Banner afbeelding" />
 
 <section id="login-container" class="flex justify-center">
-	<form id="form-box" class="mb-col-12 col-6 flex justify-center">
+	<form id="form-box" class="mb-col-12 col-6 flex justify-center" method="post" action="login.php">
 		<div class="mb-col-12 col-12 flex justify-center pb-30">
 			<div class="mb-col-12 col-5">
-				<label for="userName">Gebruikersnaam:</label>
-				<input id="userName" class="input" type="text" name="userName" placeholder="JohnDoe2025" />
+				<label for="email">E-mail:</label>
+				<input id="email" class="input" type="text" name="email" placeholder="email@example.com" />
 			</div>
 		</div>
 
 		<div class="mb-col-12 col-12 flex justify-center">
 			<div class="mb-col-12 col-5">
-				<label for="passwoord">Wachtwoord:</label>
-				<input id="passwoord" class="input" type="password" name="password" placeholder="Wachtwoord" />
+				<label for="password">Wachtwoord:</label>
+				<input id="password" class="input" type="password" name="password" placeholder="Wachtwoord" />
 			</div>
 		</div>
 		<div class="send-message mb-col-6 col-5 pt-30">
-			<button class="button" type="submit">Verzenden</button>
+			<button class="button" type="submit">Inloggen</button>
 		</div>
 	</form>
 </section>
