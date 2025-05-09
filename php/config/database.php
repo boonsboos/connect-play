b38b819f56a1d3c4e59dc5c77814d03141c111d5
Mingull <nielsplug@outlook.com>
2025-05-09 18:43:42 +0200

    fix classes not seeing the  with wrapper function

--- php/config/database.php
+++ php/config/database.php
@@ -1,16 +1,18 @@
 <?php
-$servername = "mariadb";
-$username = "root";
-$password = "";
-$name = "webshop";
+// Deze functie maakt een verbinding met de database en retourneert een PDO-object.
+function getConnection(): PDO|null
+{
+    $servername = "mariadb"; // de naam van de server. dit is niet localhost omdat de server in een docker container draait
+    $username = "root";
+    $password = "";
+    $name = "webshop";
 
-$db = null;
-try {
-    $db = new PDO("mysql:host=$servername;dbname=$name", $username, $password);
-    // set the PDO error mode to exception
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // zet de error mode naar exception
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // zet de fetch mode naar associative array
-    echo "Connected successfully";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
+    try {
+        $db = new PDO("mysql:host=$servername;dbname=$name", $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // zet de error mode naar exception
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // zet de fetch mode naar associative array
+        return $db;
+    } catch (PDOException $e) {
+        return null;
+    }
+};
