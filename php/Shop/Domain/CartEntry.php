2f641295c9de97a77437ddc5bb4aa0851e9e86e2
mastermooss <mastersmoos@gmail.com>
2025-05-12 20:22:42 +0200

    Added instal for composer / Added phpunit-test cartEntryTest

--- php/Shop/Domain/CartEntry.php
+++ php/Shop/Domain/CartEntry.php
@@ -2,21 +2,34 @@
 
 class CartEntry {
     private int $copies;
+    private bool $workshopEnabled = false;
 
-    function __construct($copies)
+    function __construct(int $copies = 0) // hiermee is de default waarde van $copies op 0 gezet
     {
      $this->copies = $copies;   
     }
     
     public function addCopy (){
-
+        $this->copies++;
     }
 
     public function removeCopy() {
-
+        if ($this->copies > 0) {
+            $this->copies--;
+        }
+    }
+    
+    public function getCopies(){
+        return $this->copies;
     }
 
-    public function toggleWorkshop() {
+    public function toggleWorkshop(): void {
+        // zodra de geburiker op de toggle knop druk zal de booleean status veranderen
+        $this->workshopEnabled = !$this->workshopEnabled;
+    }
 
+    public function isWorkshopEnabled(): bool {
+        // geeft de boolean terug van de workshop
+        return $this->workshopEnabled;
     }
 }
