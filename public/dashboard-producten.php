4859b2ee8ec2ffebbbea601db637165b8f5c5bba
boons <57451966+boonsboos@users.noreply.github.com>
2025-05-13 21:28:48 +0200

    add dashboard pages (#57)

---
+++ public/dashboard-producten.php
@@ -0,0 +1,16 @@
+<?php
+
+//require_once "/var/www/php/Shared/employee-only.php";
+require_once "/var/www/php/Shared/header.php";
+?>
+
+<img class="banner-img" src="images/bannerImg.jpg" alt="Banner afbeelding" />
+<h1 class="text-center">Producten</h1>
+
+<section id="producten-container">
+
+</section>
+
+
+<?php
+require_once "/var/www/php/Shared/footer.php";
