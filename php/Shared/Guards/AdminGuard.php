7b0299fca314c47469afa0d4e0e41c1e06c9c8a5
boons <57451966+boonsboos@users.noreply.github.com>
2025-05-15 23:51:41 +0200

    feature: add guard functionality to manage access control (#61)
    * add guard funcitonality to manage access control
    
    * add documentation comments
    
    * add die to final function
    
    * remove redundant redirects

---
+++ php/Shared/Guards/AdminGuard.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once "/var/www/php/Shared/Guards/Guard.php";
+require_once "/var/www/php/Profile/DataAccess/UserRepository.php";
+
+class AdminGuard extends Guard {
+
+    public function allowed(): bool
+    {
+        if (!isset($_SESSION["userId"])) {
+            return false;
+        }
+
+        // check dat de gebruiker administrator is
+        try {
+            $userRepo = new UserRepository();
+            $user = $userRepo->getUser($_SESSION["userId"]);
+
+            if ($user->getRole() === UserRole::ADMINISTRATOR) {
+                return true;
+            }
+        } catch (Exception $e) {
+            // TODO: logging
+        }
+
+        // als we een error krijgen of de gebruiker is geen administrator,
+        // is de gebruiker niet toegestaan
+        return false;
+    }
+}
+
