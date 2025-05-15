7b03652aeabd35add3608dd86e33180e14ee7c2d
boonsboos <mrsherobrinenaomi@gmail.com>
2025-05-15 17:27:29 +0200

    add documentation comments

--- php/Shared/Guards/Guard.php
+++ php/Shared/Guards/Guard.php
@@ -2,9 +2,19 @@
 
 abstract class Guard
 {
+    /**
+     * Subklasses moeten deze check zelf implementeren.
+     *
+     * @return bool true als de gebruiker is toegestaan, false als dat niet zo is.
+     */
     public abstract function allowed(): bool;
 
-    public function fallbackIfNotAllowed(string $location = "/login.php"): void
+    /**
+     * Redirect een gebruiker naar een pagina als ze niet toegestaan zijn.
+     * @param string $location de pagina waar naar geredirect wordt.
+     * @return void
+     */
+    public function redirectIfNotAllowed(string $location = "/login.php"): void
     {
         if (!$this->allowed()) {
             header("Location: $location");
