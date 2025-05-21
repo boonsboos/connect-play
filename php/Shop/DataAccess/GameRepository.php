d600d3b1b5ec86c103a698ca7ca799de31cd1e8c
mastermooss <mastersmoos@gmail.com>
2025-05-21 10:20:25 +0200

    reslove commits

--- php/Shop/DataAccess/GameRepository.php
+++ php/Shop/DataAccess/GameRepository.php
@@ -10,7 +10,7 @@ class GameRepository
     {
         try {
             $this->db = Database::connect();
-        } catch (PDOException $e) {
+        } catch (PDOException $e) { // vang alle fouten op die tijdens de database connectie gebeuren
             echo "Fout bij het verbinden met de database: " . $e->getMessage();
             exit;
         }
@@ -18,24 +18,7 @@ class GameRepository
 
     public function addGame(Game $game)
     {
-        try { 
-            // gebruik de prepare() ipv query() om sql injectie te voorkomen. Zo komt de invoer niet direct in de query
-            $stmtGame = $this->db->prepare("SELECT `game`.`name` FROM `game`");
-
-            $stmtGame->execute();
-
-            // geeft een array van alle opgehaalde namen van games
-            $gameNames = $stmtGame->fetchAll();
-
-            // loop door alle game namen heen om te controleren of de naam al bestaat
-            foreach ($gameNames AS $gameName) {
-                if ($gameName['name'] === $game->getName()) {
-                    // bestaat de naam al gooi dna een Exception
-                    throw new Exception("Game naam bestaat al!");
-                }
-            }
-            
-            // gebruik de prepare() ipv query() om sql injectie te voorkomen. Zo komt de invoer niet direct in de query
+        try {
             $stmtGame = $this->db->prepare("CALL add_game(:players, :price, :duration, :name, :description, :difficulty, :left_in_stock)");
 
             $stmtGame->execute([
@@ -54,35 +37,33 @@ class GameRepository
                 $game->setId((int) $gameRow['game_id']);
             }
 
-        } catch (Exception $e) {
-            // laat het error bericht zien
-            echo $e->getMessage();
-            die();
+        } catch (PDOException $e) {
+            if ($e->getCode() === '23000') { // Code 23000 betekent "Integrity constraint violation". je probeert iets toe te voegen dat de db verbied, zoals dubbele game namen
+                throw new Exception("Game naam bestaat al!");
+            }
+            throw $e;
         }
     }
 
     public function getGames(): array  {
         $allGames = [];
 
-        // gebruik de prepare() ipv query() om sql injectie te voorkomen. Zo komt de invoer niet direct in de query
         $stmtGame = $this->db->prepare("SELECT * FROM `game`");
 
         $stmtGame->execute();
 
-        // geeft een array van alle opgehaalde rijen (games)
         $gameRows = $stmtGame->fetchAll();
 
-        // loop door alle rijen heen en maakt per rij een Game object aan en stopt deze gegevens in een array
         foreach ($gameRows as $row) {
-            // Soms zet PDO opgehaalde waarden om naar strings. Dit kan typefouten veroorzaken.
+            // PDO retourneert standaard alle kolommen als string, dit kan typefouten veroorzaken.
             // Daarom wordt de waarde hier duidelijk teruggezet naar het oorspronkelijke type.
             $allGames[] = new Game(
                 (int) $row['players'],
                 (float) $row['price'],
                 (int) $row['duration'],
-                $row['name'],
-                $row['description'],
-                $row['difficulty'],
+                (string) $row['name'],
+                (string) $row['description'],
+                (string) $row['difficulty'],
                 (int) $row['left_in_stock'],
                 (int) $row['game_id']
             );
