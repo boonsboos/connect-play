a87211b475aa4ea5992ea3e5b34dc4133f6d9a46
boonsboos <boons@boonsboos.nl>
2025-05-27 00:48:18 +0200

    fix filters and pagination DOM position
    Not the very best way to do it, you might want to do it on the
    database instead, but this is more than fine for now.
    
    This commit also adds a user friendly message that signals when
    a search query turns up no results. Now we have an easy way
    to hook in when that happens, and return this to our admin.

--- php/Shop/Controllers/WebshopController.php
+++ php/Shop/Controllers/WebshopController.php
@@ -1,10 +1,18 @@
 <?php
 
-require_once '../php/Shop/DataAccess/WebshopRepository.php';
+require_once '/var/www/php/Shop/DataAccess/GameRepository.php';
 
 class WebshopController {
 
-    private WebshopRepository $webshopRepository;
+    private GameRepository $gameRepository;
+
+    private string $searchQuery;
+    private int $maxPlayers = 16;
+    private int $minPlayers = 2;
+
+    private bool $filterActive = false;
+
+    private array $games = [];
 
     /**
      * Aantal spellen per pagina
@@ -13,7 +21,7 @@ class WebshopController {
 
     public function __construct() {
         // Aanmaken van een instantie van de WebshopRepository class om databaseoperaties uit te voeren
-        $this->webshopRepository = new WebshopRepository();
+        $this->gameRepository = new GameRepository();
     }
 
     /**
@@ -24,27 +32,97 @@ class WebshopController {
         return isset($_GET['page']) ? (int)$_GET['page'] : 1;
     }
 
-    public function getGames(int $amountOfGames = 0): array {
-        // Als er geen limiet wordt meegegeven, houd de standaard limiet aan van 6
-        if ($amountOfGames <= 0) {
-            $amountOfGames = $this->gamesPerPage;
-        } else /* if ($limit > 0) */ {
+    public function getGames(): array
+    {
+        // We moeten 1 aftrekken van de huidige pagina zodat de offset goed staat
+        // (1 - 1) = 0 * 6 = de eerste 6 items
+        // (2 - 1) = 1 * 6 = de tweede 6 items
+        $offset = ($this->getCurrentPage() - 1) * $this->gamesPerPage;
+
+        // Haal de slice op
+        return array_slice(
+            $this->games,
+            $offset,
+            $this->gamesPerPage
+        );
+    }
+
+    public function fetchGames(int $amountOfGames = 0): void {
+        // Als er een custom limiet wordt meegegeven, houd die aan, gebruik anders de standaard
+        if ($amountOfGames > 0) {
             $this->gamesPerPage = $amountOfGames;
+        } else {
+            $this->gamesPerPage = 6;
         }
 
-        // Haal op op welke pagina we zitten
-        $currentPage = $this->getCurrentPage();
+        // Sla de games voor deze pagina op
+        $this->games = $this->gameRepository->getGames();
 
-        // Geef de juiste hoeveelheid games terug van de juiste pagina
-        return $this->webshopRepository->getAllGames($currentPage, $amountOfGames);
+        // Sla de gefilterde games op
+        $this->games = $this->filterGames($this->games);
     }
 
     public function getTotalOfGames(): int {
-        return $this->webshopRepository->getTotalGames();
+        return count($this->games);
     }
 
+    /** Rondt naar boven af hoeveel pagina's aan games er zijn
+     * @return int het aantal pagina's
+     */
     public function getTotalPages(): int {
         return ceil($this->getTotalOfGames() / $this->gamesPerPage);
+    }
+
+    public function setFilterSettings(array $filterSettings): void
+    {
+        $this->filterActive = true;
+        $this->searchQuery = $filterSettings['search'] ?? '';
+        $this->maxPlayers = $filterSettings['maxspelers'];
+        $this->minPlayers = $filterSettings['minspelers'];
+    }
+
+    /**
+     * Filtert de games op basis van de ingestelde filters
+     * @param Game[] $games
+     * @return Game[]
+     */
+    private function filterGames(array $games): array
+    {
+        // als de filter niet actief is, sla filteren over
+        if (!$this->filterActive) {
+            return $games;
+        }
+
+        // filter op min spelers
+        $games = array_filter($games, function($game) {
+            return $game->getPlayers() >= $this->minPlayers;
+        });
+
+        // filter op max spelers
+        $games = array_filter($games, function($game) {
+            return $game->getPlayers() <= $this->maxPlayers;
+        });
+
+        // filter op zoekopdracht
+        $games = array_filter($games, function($game) {
+            // allebei in lowercase zodat hoofdletters niet uitmaken
+            return str_contains(
+                strtolower($game->getName()),
+                strtolower($this->searchQuery)
+            );
+        });
+
+        return $games;
+    }
+
+
+    public function getFilterParams(): string
+    {
+        if ($this->filterActive) {
+            return "&search=$this->searchQuery&maxspelers=$this->maxPlayers&minspelers=$this->minPlayers";
+        }
+
+        return "";
     }
 }
 ?>
--- public/webshop.php
+++ public/webshop.php
@@ -1,63 +1,94 @@
-<script src="/js/filter.js"></script>
-
 <?php
 // Inclusie van noodzakelijke bestanden
 require_once '../php/Shared/header.php';
 
 require_once '../php/Shop/controllers/WebshopController.php';
 
-$controller = new WebshopController(); // limit : 6
+$controller = new WebshopController();
 
 $currentPage = $controller->getCurrentPage();
+
+if (isset($_GET["search"]) && isset($_GET["maxspelers"]) && isset($_GET["minspelers"])) {
+	$controller->setFilterSettings($_GET);
+}
+
+$controller->fetchGames();
 ?>
 
 <img class="banner-img" src="images/bannerImg.jpg" alt="Banner afbeelding" />
-<div class="webshop-container">
-    <div class="col-2 productfilter flex justify-center">
+<div class="webshop-container mb-flex-col">
+    <div class="col-2 productfilter product-accent-border flex justify-center">
         <aside class="filter-sectie">
-            <h3>Filter:</h3>
-            <form>
-                <label for="search">Zoek Product:</label>
-                <input type="text" id="zoekfunctie" name="search" placeholder="Zoek Product">
-                <h4>Soort product:</h4>
-                <label><input type="checkbox" name="bordspellen"> Bordspellen</label><br>
-                <label><input type="checkbox" name="kaartspellen"> Kaartspellen</label><br>
-                <label><input type="checkbox" name="workshops"> Workshops</label>
-                <h4>Leeftijdsgrens:</h4>
-                <input type="range" name="age" min="0" max="99">
-                <button type="submit">Filter nu</button>
+            <h3>Filteren</h3>
+            <form class="flex flex-col">
+				<div>
+					<label for="zoekfunctie">Zoek Product:</label>
+					<input type="text" id="zoekfunctie" name="search" placeholder="Zoek Product" value="<?php echo $_GET["search"] ?? ""; ?>">
+				</div>
+				<div>
+					<label for="maxspelers">Max. Spelers</label>
+					<input type="range" id="maxspelers" name="maxspelers" min="2" max="16" step="2" value="<?php echo $_GET["maxspelers"] ?? '16'; ?>">
+				</div>
+				<div>
+					<label for="minspelers">Min. Spelers</label>
+					<input type="range" id="minspelers" name="minspelers" min="2" max="16" step="2" value="<?php echo $_GET["minspelers"] ?? '2'; ?>">
+				</div>
+				<div>
+                	<button class="button" type="submit">Filter nu</button>
+				</div>
             </form>
         </aside>
     </div>
-    <div class="productlijst mb-col-12 col-9 flex justify-center">
-         <!-- Productlijstweergave -->
-        <?php foreach ($controller->getGames() as $game): ?> <!-- // limit : 6 -->
-            <div class="game-card">
-                <!-- Weergave van een individuele game-kaart -->
-                <h3><?php echo htmlspecialchars($game->getName()); ?></h3>
-                <p>Prijs: €<?php echo htmlspecialchars($game->getPrice()); ?></p>
-                <p>Spelers: <?php echo htmlspecialchars($game->getPlayers()); ?></p>
-                <p>Beschrijving: <?php echo htmlspecialchars($game->getDescription()); ?></p>
-                <hr/>
-            </div>
-        <?php endforeach; ?>
+	<div class="product-accent-border mb-col-12 col-9">
 
-        <!-- Paginering -->
-        <div class="pagination">
-            <!-- Wanneer pagina groter dan 1 is, link naar vorige pagina-->
-        <?php if ($currentPage > 1): ?>
-            <a href="?page=<?php echo $currentPage - 1; ?>">Vorige</a>
-        <?php endif; ?>
-        <!-- Berekenen van het aantal pagina's -->
-        <?php for ($i = 1; $i <= $controller->getTotalPages(); $i++): ?> <!-- // limit : 6 -->
-            <a href="?page=<?php echo $i; ?>" class="<?php echo $i === $currentPage ? 'active' : ''; ?>"><?php echo $i; ?></a>
-        <?php endfor; ?>
-        <!-- Wanneer de huidige pagina kleiner is dan het totaal aantal pagina's, link naar volgende pagina-->
-        <?php if ($currentPage < $controller->getTotalPages()): ?> <!-- // limit : 6 -->
-            <a href="?page=<?php echo $currentPage + 1; ?>">Volgende</a>
-        <?php endif; ?>
-    </div>
-    </div>
+        <?php if ($controller->getTotalOfGames() == 0): ?>
+		<div class="flex justify-center align-center py-50">
+			<h1 class="text-center">We hebben het product dat je zoekt niet kunnen vinden... :(</h1>
+		</div>
+        <?php endif;?>
+
+		<div class="productlijst mb-col-12 col-9 flex justify-center">
+			 <!-- Productlijstweergave -->
+
+			<?php foreach ($controller->getGames() as $game): ?> <!-- // limit : 6 -->
+				<div class="game-card">
+					<!-- Weergave van een individuele game-kaart -->
+					<h3><?php echo htmlspecialchars($game->getName()); ?></h3>
+					<p>Prijs: €<?php echo htmlspecialchars($game->getPrice()); ?></p>
+					<p>Spelers: <?php echo htmlspecialchars($game->getPlayers()); ?></p>
+					<p>Beschrijving: <?php echo htmlspecialchars($game->getDescription()); ?></p>
+					<hr/>
+				</div>
+			<?php endforeach; ?>
+		</div>
+
+		<!--
+			Elke link naar de volgende pagina moet de filter parameters meegeven
+		-->
+
+		<!-- Paginering -->
+		<div class="pagination py-10 px-10">
+			<!-- Wanneer pagina groter dan 1 is, link naar vorige pagina-->
+			<?php if ($currentPage > 1): ?>
+				<a href="?page=<?php echo $currentPage - 1 . $controller->getFilterParams(); ?>">Vorige</a>
+			<?php endif; ?>
+			<!-- Berekenen van het aantal pagina's -->
+			<?php for ($i = 1; $i <= $controller->getTotalPages(); $i++): ?>
+				<a href="?page=<?php echo $i . $controller->getFilterParams(); ?>"
+				<!-- make current page bold-->
+				<?php if ($i === $currentPage): ?>
+					<b><u><?php echo $i; ?></u></b>
+				<?php else: ?>
+					<?php echo $i; ?>
+				<?php endif; ?>
+				</a>
+			<?php endfor; ?>
+			<!-- Wanneer de huidige pagina kleiner is dan het totaal aantal pagina's, link naar volgende pagina-->
+			<?php if ($currentPage < $controller->getTotalPages()): ?>
+				<a href="?page=<?php echo $currentPage + 1 . $controller->getFilterParams(); ?>">Volgende</a>
+			<?php endif; ?>
+		</div>
+	</div>
 </div>
 
 <?php require_once '../php/Shared/footer.php'; ?>
