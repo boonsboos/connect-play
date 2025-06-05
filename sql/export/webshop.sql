422d292749910129d9bbcecdedd12a581fd56791
mastermooss <60898036+mastermooss@users.noreply.github.com>
2025-06-05 11:07:52 +0200

    Added the create database (#88)

--- sql/export/webshop.sql
+++ sql/export/webshop.sql
@@ -20,6 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Database: `webshop`
 --
+DROP DATABASE IF EXISTS `webshop`;
+CREATE DATABASE IF NOT EXISTS `webshop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `webshop`;
 
 DELIMITER $$
 --
