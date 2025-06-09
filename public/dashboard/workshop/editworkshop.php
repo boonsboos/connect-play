2786dadd400f79f3246313b83805a5c6cb37d887
boonsboos <mrsherobrinenaomi@gmail.com>
2025-06-09 12:14:54 +0200

    create files

---
+++ public/dashboard/workshop/editworkshop.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once "/var/www/php/Shared/header.php";
+?>
+
+<img class="banner-img" src="/images/bannerImg.jpg" alt="Banner afbeelding" />
+
+<section id="workshop-container">
+    <h1 class="text-center">Workshop bewerken</h1>
+
+
+</section>
+
+<?php
+require_once "/var/www/php/Shared/footer.php";
