95299514632234a730c89af2689a457889d78bc4
boonsboos <boons@boonsboos.nl>
2025-06-12 23:50:40 +0200

    dashboardrepo is a controller

--- php/Shop/Controllers/DashboardController.php
+++ php/Shop/Controllers/DashboardController.php
@@ -1,6 +1,6 @@
 <?php
 
-class DashboardRepository
+class DashboardController
 {
     private array $employeeButtons;
     private array $adminButtons;
--- public/dashboard.php
+++ public/dashboard.php
@@ -1,9 +1,9 @@
 <?php
 
 require_once '../php/Shared/header.php'; 
-require_once '../php/Shop/DataAccess/DashboardRepository.php';
+require_once '../php/Shop/Controllers/DashboardController.php';
 
-$controller = new DashboardRepository();
+$controller = new DashboardController();
 ?>
 
 <img class="banner-img" src="/images/bannerImg.jpg" alt="Banner afbeelding" />
@@ -21,10 +21,10 @@ $controller = new DashboardRepository();
         <?php endif; ?>
 
         <?php if ($user->getRole() === UserRole::ADMINISTRATOR): ?>
-            <!-- Scheidingslijn tussen de rollen. Alleen zichtbaar wanneer admin inlog -->
-            <hr class="lineBetweenRoles col-12"></hr>
+            <!-- Scheidingslijn tussen de rollen. Alleen zichtbaar wanneer admin inlogt -->
+            <hr class="lineBetweenRoles col-12">
+
             <div class="administratorBox flex justify-center p-30">
-                <!-- Met alleen de rol ADMINISTRATOR wordt de sectie administrator getoond -->
                 <p class="dasboardSectionTitel col-12 pb-30">ADMINISTRATOR</p>
                 <?php foreach($controller->getAdminButtons() as $button): ?>
 					<a class="dashboardButton col-3 text-center" href="<?= $button['url'] ?>"> <?= __($button['name']) ?></a>
