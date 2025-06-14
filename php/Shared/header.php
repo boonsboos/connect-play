0f551d73d5ad8200268b058822661a019d86ca1f
Tippe van Roosmalen <tippevanroosmalen@outlook.com>
2025-06-14 13:08:56 +0200

    Fixed issue where shoppingCart still shows stuff when not logged in

--- php/Shared/header.php
+++ php/Shared/header.php
@@ -10,7 +10,6 @@ function __($key): string // verkorte versie voor de vertaal functie
     return $language->translate($key); // vertaal de sleutel naar de huidige taal
 }
 
-
 // check of userId in de sessie zit
 if (isset($_SESSION["userId"])) {
     $userId = $_SESSION["userId"];
@@ -23,6 +22,8 @@ if (isset($_SESSION["userId"])) {
         header("Location: /login.php", true, 303);
         die();
     }
+} else {
+    $userId = 0;
 }
 
 ?>
